b42ff3d3f70281ac3e435f9c14ce388da2f722cd
adm-thiagomelo <ThiagoMeloDoc@gmail.com>
2026-08-20 11:54:42 -0300

    Atualizacao 20/08 manual

--- Exercicios/rendimento_taxista.py
+++ Exercicios/rendimento_taxista.py
@@ -3,20 +3,21 @@
 odometro_inicio = float(input('Odômetro (KM); Início: '))
 odometro_fim = float(input('Odômetro (KM); Final: '))
 
-combustivel_gasto = float(int(input('Combustível (L): ')))
+total_recebido = float(input('Total Recebido (R$): '))
 
-total_recebido = float(int(input('Total Recebido (R$): ')))
+combustivel_gasto = float(input('Combustível Utilizado (L): '))
 
 PRECO_COMBUSTIVEL = 6.15
 
-
-
 # PROCESSAMENTO
 
+gastos_dia = combustivel_gasto * PRECO_COMBUSTIVEL
+lucro_liquido = total_recebido - gastos_dia
+
 kms_corridos = odometro_fim - odometro_inicio
-custo_dia = combustivel_gasto * PRECO_COMBUSTIVEL
-lucro_liquido = total_recebido - custo_dia
+media_kml = kms_corridos / combustivel_gasto
 
 # SAÍDA
 
-print(f'Lucro Líquido (R$): R${lucro_liquido}')
+print(f'Lucro Líquido: R${lucro_liquido}')
+print(f'Consumo Médio (KM/L): {media_kml}')
