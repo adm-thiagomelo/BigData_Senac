f608161425dcd6251267ac1370a598729b567761
adm-thiagomelo <ThiagoMeloDoc@gmail.com>
2026-08-20 11:00:16 -0300

    Atualizacao 20/08

---
+++ Exercicios/media_optativa.py
@@ -0,0 +1,33 @@
+# ENTRADA
+
+nota_um = int(input('Primeira Nota: '))
+nota_dois = int(input('Segunda Nota:'))
+nota_optativa = int(input('Nota da Optativa: '))
+
+# PROCESSAMENTO
+
+if nota_optativa != -1:
+
+    if nota_um > nota_dois and nota_optativa > nota_dois:
+        nota_dois = nota_optativa        
+    # elif nota_um == nota_dois:    # Previsto, mas não fará diferença.
+    elif nota_um < nota_dois and nota_optativa > nota_um:
+        nota_um = nota_optativa
+
+    media = (nota_um + nota_dois) / 2
+
+else:
+    media = (nota_um + nota_dois) / 2
+
+if media >= 6:
+    situacao = "Aprovação!"
+elif media >= 3 and media < 6:
+    situacao = "Recuperação!"
+else:
+    situacao = "Reprovação!"
+
+
+# SAÍDA
+
+print(f'Média: {media}')
+print(f'Situação: {situacao}')
