62342ef8a2ceb5fa5fe0e2d2ccdf71e829f16429
adm-thiagomelo <ThiagoMeloDoc@gmail.com>
2026-08-22 10:54:47 -0300

    Atualizacao 22/08

--- Exercicios/cadastro_candidatos.py
+++ Exercicios/cadastro_candidatos.py
@@ -11,21 +11,12 @@ ano_presente = 2026
 for num_candidato in range(1, 13):
     print(f'Candidato {num_candidato}')
 
-    try:
-        ano_nascimento = int(input('Ano de Nascimento: '))
-    except ValueError:
-        print('Por favor, insira apenas números.')
-
+    ano_nascimento = int(input('Ano de Nascimento: '))    
     idade = ano_presente - ano_nascimento
 
     if idade >= 18:
-        try:
-            telefone = int(input('Telefone: '))
-        except ValueError:
-            print('Por favor, insira apenas números.')
-
-        email = str(input('Correio Eletrônico: '))
-
+        telefone = input('Telefone: ')
+        email = input('Correio Eletrônico: ')
     else:
         print('Sua idade é insuficiente!')
 
