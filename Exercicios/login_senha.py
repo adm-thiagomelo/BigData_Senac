4208b94b5fab9132dc05b3aad54a59bfaa20f6e9
adm-thiagomelo <ThiagoMeloDoc@gmail.com>
2026-08-22 11:23:34 -0300

    Atualizacao 22/08

--- Exercicios/login_senha.py
+++ Exercicios/login_senha.py
@@ -10,3 +10,27 @@
 # ● Se as tentativas acabarem, imprima uma mensagem de bloqueio
 
 # usar for
+
+
+login_correto = 'douglas'
+senha_correta = 'abobora'
+
+# ENTRADA
+
+for tentativa in range(1, 4):
+    print(f'Tentativa ({tentativa}/3)')
+    print()
+
+    login = input('Login: ')
+    senha = input('Senha: ')
+    print()
+
+    if login == login_correto and senha == senha_correta:
+        print('Bem-vindo, Douglas!')
+        break
+    else:
+        print('Usuário ou senha incorretos!')
+        print()
+
+if login != login_correto and senha != senha_correta:
+    print('Bloqueio!')
