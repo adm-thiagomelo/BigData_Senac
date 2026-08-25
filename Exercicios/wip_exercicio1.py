4996ca908582b9063263b42c3451e98ab23082af
THIAGO MELO DE OLIVEIRA <melo.thiago@aluno.senac.rio>
2026-08-24 21:44:45 -0300

    Aula 24/08

---
+++ Exercicios/wip_exercicio1.py
@@ -0,0 +1,4 @@
+# Média Escolar para 5 Estudantes
+# Use um for loop para iterar 5 vezes. Dentro do loop, realize a leitura das notas e a decisão
+# (if/elif/else) da média. Crie uma lista vazia (resultados = []). A cada repetição, adicione uma
+# string (ex: "Aluno 1 - Aprovado") a esta lista usando .append().
---
+++ Exercicios/wip_exercicio2.py
@@ -0,0 +1,5 @@
+# Cadastro Seletivo de Candidatos
+# Use um for loop para iterar 5 vezes. Dentro do loop, use um if/else para checar se o
+# candidato é menor de 18 anos (rejeição). Crie uma lista principal: candidatos_validos = [].
+# Se o candidato for válido, crie um Dicionário (ex: candidato = {'nome': '...', 'email': '...'}).
+# Adicione este Dicionário à lista: candidatos_validos.append(candidato)
