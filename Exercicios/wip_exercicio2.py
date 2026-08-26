f4df39a0d894eeb0008ccd30b8ac0a2b67cc2951
THIAGO MELO DE OLIVEIRA <melo.thiago@aluno.senac.rio>
2026-08-26 11:49:49 -0300

    Atualizacao 26/08

--- Exercicios/wip_exercicio2.py
+++ Exercicios/wip_exercicio2.py
@@ -3,3 +3,24 @@
 # candidato é menor de 18 anos (rejeição). Crie uma lista principal: candidatos_validos = [].
 # Se o candidato for válido, crie um Dicionário (ex: candidato = {'nome': '...', 'email': '...'}).
 # Adicione este Dicionário à lista: candidatos_validos.append(candidato)
+
+candidatos_validos = []
+
+for i in range(1, 6):
+    print(f'Candidato {i}')
+
+    idade = int(input('Idade: '))
+    print()
+
+    if idade < 18:
+        print('Rejeição')
+        print()
+    else:
+        imprimir_lista = True
+
+        nome = input('Nome: ')
+        email = input('Endereço Eletrônico: ')
+
+        candidato = {'Nome': nome, 'Email': email}
+        candidatos_validos.append(candidato)
+        print()
