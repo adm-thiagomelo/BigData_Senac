52d5519fc2ad56f24d4fe22d7d04332bc4866af7
THIAGO MELO DE OLIVEIRA <melo.thiago@aluno.senac.rio>
2026-09-21 19:10:43 -0300

    Aula 21/09

--- Exercicios/aula140926_wb.sql
+++ Exercicios/aula140926_wb.sql
@@ -17,16 +17,57 @@ estoque INT
 -- REMOVER TABELA/CONTEÚDO DE TABELA
 DROP TABLE Produtos;
 
---
-SET GLOBAL local_infile = 1; -- marcação de aceite para arquivos locais (passo extra 01 junto ao load data)
+-- IMPORTAÇÃO VIA CÓDIGO NÃO FUNCIONOU
 
--- 'OPT_LOCAL_INFILE=1' -- (passo extra 02 junto ao load data) inserir na sua conexão local (edit da conexão >> Advanced >> Others)
+-- CRIANDO A ENTIDADE CLIENTE:
+CREATE TABLE Clientes (
+id_cliente VARCHAR(10),
+nome VARCHAR(100),
+email VARCHAR(30)
+);
 
-LOAD DATA LOCAL INFILE 'C:\\Users\\melo.thiago\\Documents\\bigData\\BigData_Senac\\Exercicios\\vendas_produtos.csv' -- Ajuste o caminho no seu banco local
-INTO TABLE meu_ecommerce.produtos
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n' -- Aqui: CR LF
-IGNORE 1 ROWS -- Pula o cabeçalho 'id_produto,nome...'
-(id_produto, nome, categoria, @preco_var, estoque) -- Mapeia colunas
-SET preco = REPLACE(@preco_var, '.', '.'); -- Garante que o decimal seja lido corretamente
+-- CRIANDO A ENTIDADE PROMOVIDA PEDIDOS:
+
+CREATE TABLE Pedidos (
+id_pedido VARCHAR(10),
+id_cliente VARCHAR(10),
+data_pedido DATE,
+valor_total DECIMAL(10,2),
+id_produto VARCHAR(10),
+quantidade SMALLINT
+);
+
+-- OPÇÕES DE CONSTRUÇÃO DE CHAVES:
+-- 1 - DESDE O CREATE TABLE
+
+CREATE TABLE Pedidos (
+id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+data_pedido DATE,
+valor_total DECIMAL(10,2),
+id_cliente INT,
+id_produto INT,
+quantidade INT,
+FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+);
+
+-- 2 - A PARTIR DO ALTER TABLE:
+ALTER TABLE pedidos
+ADD CONSTRAINT fk_pedidos_clientes
+FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente);
+
+ALTER TABLE pedidos
+ADD CONSTRAINT fk_pedidos_produtos
+FOREIGN KEY (id_produto) REFERENCES produtos(id_produto);
+
+ALTER TABLE produtos
+ADD CONSTRAINT pk_produtos
+PRIMARY KEY (id_produto);
+
+ALTER TABLE clientes
+ADD CONSTRAINT pk_clientes
+PRIMARY KEY (id_cliente);
+
+ALTER TABLE pedidos
+ADD CONSTRAINT pk_pedidos
+PRIMARY KEY (id_pedido);
